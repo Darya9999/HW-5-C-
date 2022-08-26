2f4a0394d713721e576ec837cb8fc91526067314
Darya <darya.ogurtsova.99@mail.ru>
2022-08-26 16:53:24 +0300

    Добавлено задание 2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,24 @@
+﻿public class HelloWorld
+{
+public static void Main(string[] args)
+{
+int[] arr = new int[6];
+Random rnd = new Random(999);
+string otv = "";
+int otvet = 0;
+
+for(int i = 0; i < 6; i++){
+arr[i] = rnd.Next(-99, 99);
+if (i % 2 == 1) {
+otvet+=arr[i];
+}
+otv += arr[i];
+if (i<=4) {
+otv += ",";
+}
+}
+otv = "[" + otv + "] -> " + otvet;
+
+Console.WriteLine (otv);
+}
+}
